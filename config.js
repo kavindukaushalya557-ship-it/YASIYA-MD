1df3ee5078a73ae868f678c0d10765d9ac4f895d
kavindukaushalya557-ship-it <kavindukaushalya557@gmail.com>
2025-08-18 15:58:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~4Ew0XTrL#u9Vl6ikluc_TifWNTHRRdp9Xe9R5uhlROUaB7KE67DQ'
 };
